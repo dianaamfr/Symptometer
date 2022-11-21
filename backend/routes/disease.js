178846e717350edbd79fec1f2f051eb2ee2d1c55
Diana Freitas <diana.amfreitas@gmail.com>
2022-11-21 14:34:39 +0000

    Add disease with all symptoms query

--- backend/routes/disease.js
+++ backend/routes/disease.js
@@ -4,7 +4,8 @@ const axios = require("axios");
 require("dotenv").config();
 
 const ontologyUrl = process.env.FUSEKI_URL;
-const prefix = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n\
+const prefix =
+  "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n\
                 PREFIX owl: <http://www.w3.org/2002/07/owl#> \n\
                 PREFIX doid: <http://purl.obolibrary.org/obo/>";
 
@@ -16,9 +17,12 @@ router.use(function (req, res, next) {
 
 router.get("/bySymptoms", async (req, res) => {
   let symptoms = JSON.parse(req.query.symptoms);
-  symptoms = symptoms.map((e) => { return `'${e}'`;});
+  symptoms = symptoms.map((e) => {
+    return `'${e}'`;
+  });
   const symptomsValues = symptoms.join(" ");
-  const query = prefix +
+  const query =
+    prefix +
     ` SELECT ?diseaseName \n\
       WHERE { \n\
       ?diseaseID rdfs:label ?diseaseName . \n\
@@ -46,5 +50,49 @@ router.get("/bySymptoms", async (req, res) => {
       return res.json(err);
     });
 });
+
+router.get("/byAllSymptoms", async (req, res) => {
+  let symptoms = JSON.parse(req.query.symptoms);
+  symptoms = symptoms.map((e) => {
+    return `'${e}'`;
+  });
+  const symptomsQuery = buildSymptomsQuery(symptoms);
+
+  const query =
+    prefix +
+    ` SELECT ?diseaseName \n\
+      WHERE { \n\
+      ?diseaseID rdfs:label ?diseaseName . \n\
+      ${symptomsQuery}
+    } GROUP BY ?diseaseName \n\
+    ORDER BY DESC(COUNT(?symptomID))`;
+
+  await axios({
+    url: ontologyUrl,
+    method: "GET",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      Accept: "application/sparql-results+json",
+    },
+    params: { query: query },
+  })
+    .then((jenaResponse) => {
+      return res.json(jenaResponse.data);
+    })
+    .catch((err) => {
+      return res.json(err);
+    });
+});
+
+function buildSymptomsQuery(symptoms) {
+  return symptoms
+    .map((symptom, i) => {
+      return `?diseaseID rdfs:subClassOf ?restriction${i} . \n\
+    ?restriction${i} owl:onProperty doid:RO_0002452 . \n\
+    ?restriction${i} owl:someValuesFrom ?symptomID${i} . \n\
+    ?symptomID${i} rdfs:label ${symptom} . \n\ `;
+    })
+    .join("");
+}
 
 module.exports = router;
